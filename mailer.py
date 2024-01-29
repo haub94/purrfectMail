c14511c1eaaac6b9e44513ba6dde336acab05021
Markus Haubold <markus@ostec.de>
2024-01-29 20:50:31 +0100

    use .env to store credentials

--- mailer.py
+++ mailer.py
@@ -1,10 +1,15 @@
 import smtplib
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 #Email Variables
 SMTP_SERVER = 'smtp.gmail.com' #Email Server (don't change!)
 SMTP_PORT = 587 #Server Port (don't change!)
-GMAIL_USERNAME = '' #change this to match your gmail account
-GMAIL_PASSWORD = '' #change this to match your gmail app-password
+GMAIL_USERNAME = os.getenv('GMAIL_USERNAME') #change this to match your gmail account
+GMAIL_PASSWORD = os.getenv('GMAIL_PASSWORD') #change this to match your gmail app-password
 
 class Emailer:
     def sendmail(self, recipient, subject, content):
@@ -29,7 +34,7 @@ class Emailer:
 
 sender = Emailer()
 
-sendTo = ''
+sendTo = os.getenv('SEND_TO')
 emailSubject = "Hello World"
 emailContent = "This is a test of my Emailer Class"
 
