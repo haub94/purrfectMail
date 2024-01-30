610028bd1c7594b0ac0ed8cd4e4206fa6fde8294
Markus Haubold <Markus-Haubold@web.de>
2024-01-30 16:46:59 +0100

    [main] use image from catGallery or the default cat

--- README.md
+++ README.md
@@ -5,4 +5,18 @@ Send Images from your fluffy cat per mail.
 - HTML body for mail content
 - load image local and send it
 - cronjob to mail every day 10am
-   
+
+
+
+To implement the mailing i followed the awesome tutorial from [Chris@CBR](https://bc-robotics.com/tutorials/sending-email-attached-photo-using-python-raspberry-pi/)
+
+
+
+
+
+## Default cat
+By default the mail will contain the [picture from minka](minka.jpeg).
+
+
+## Use your own cat-content!
+Add some fluffy images to the [catGallery](catGallery) folder
--- main.py
+++ main.py
@@ -1,15 +1,38 @@
+import os
+import random
 from SensibleDataHandler import SensibleDataHandler
 from Emailer import Emailer
+
+PATH_TO_IMAGE_GALLERY = 'catGallery/' #add here the images from your cat
 
 sdh = SensibleDataHandler()
 emailer = Emailer()
 
-image = "minka.jpeg"
+image = ''
+
+try:
+    # Get a list of all the image files in the folder
+    allImagesFromGallery = [f for f in os.listdir(PATH_TO_IMAGE_GALLERY) if os.path.isfile(os.path.join(PATH_TO_IMAGE_GALLERY, f))]
+
+    # Select a random image from the list
+    randomImageFromGallery = random.choice(allImagesFromGallery)
+
+    image = PATH_TO_IMAGE_GALLERY + randomImageFromGallery
+
+except:
+    print("It seems that your catGallery-folder is empty! By default Minka will be send")
+    image = "minka.jpeg" #use default
+
 recipient = sdh.getRecipient1()
 
-emailSubject = "meow...meow"
+emailSubject = "meow..."
 emailContent = "Minki of the day."
 
 #Sends an email to the "sendTo" address with the specified "emailSubject" as the subject and "emailContent" as the email content.
 
+
+print("image: ", image);
+
+
+#send the mail 
 emailer.sendmail(recipient, emailSubject, emailContent, image)
