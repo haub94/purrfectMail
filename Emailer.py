e86c40f0cdec7a2b0e705c3e761f42f2d7cbfb63
Markus Haubold <Markus-Haubold@web.de>
2024-01-30 15:56:14 +0100

    [main] add image as attachment

--- Emailer.py
+++ Emailer.py
@@ -1,22 +1,34 @@
 import smtplib
 from SensibleDataHandler import SensibleDataHandler
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
 
 sdh = SensibleDataHandler()
 
 #Email Variables
 SMTP_SERVER = 'smtp.gmail.com' #Email Server (don't change!)
 SMTP_PORT = 587 #Server Port (don't change!)
-GMAIL_USERNAME = sdh.getSenderName() 
-GMAIL_PASSWORD = sdh.getSenderAppPassword()  
+GMAIL_USERNAME = sdh.getSenderName()
+GMAIL_PASSWORD = sdh.getSenderAppPassword()
 
 class Emailer:
-    def sendmail(self, recipient, subject, content):
+    def sendmail(self, recipient, subject, content, image):
 
         #Create Headers
-        headers = ["From: " + GMAIL_USERNAME, "Subject: " + subject, "To: " + recipient,
-                   "MIME-Version: 1.0", "Content-Type: text/html"]
-        headers = "\r\n".join(headers)
+        emailData = MIMEMultipart()
+        emailData['Subject'] = subject
+        emailData['To'] = recipient
+        emailData['From'] = GMAIL_USERNAME
 
+        #Attach our text data
+        emailData.attach(MIMEText(content))
+
+        #Create our Image Data from the defined image
+        imageData = MIMEImage(open(image, 'rb').read(), 'jpeg')
+        imageData.add_header('Content-Disposition', 'attachment; filename="minki"')
+        emailData.attach(imageData)
+        
         #Connect to Gmail Server
         session = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
         session.ehlo()
@@ -27,6 +39,6 @@ class Emailer:
         session.login(GMAIL_USERNAME, GMAIL_PASSWORD)
 
         #Send Email & Exit
-        session.sendmail(GMAIL_USERNAME, recipient, headers + "\r\n\r\n" + content)
+        session.sendmail(GMAIL_USERNAME, recipient, emailData.as_string())
         session.quit
         print("email sent")
--- main.py
+++ main.py
@@ -4,10 +4,13 @@ from Emailer import Emailer
 sdh = SensibleDataHandler()
 emailer = Emailer()
 
-sendTo = sdh.getRecipient1()
+image = "minka.jpeg"
+sender = sdh.getSenderName()
+recipient = sdh.getRecipient1()
+
 emailSubject = "meow...meow"
-emailContent = " <!DOCTYPE html <html> <head> <title>Your Email Title</title> </head> <body> <h1>Hello, This is Your Email Content</h1> <p>This is a paragraph in the email.</p> </body> </html>"
+emailContent = "Hello from Minki"
 
 #Sends an email to the "sendTo" address with the specified "emailSubject" as the subject and "emailContent" as the email content.
 
-emailer.sendmail(sendTo, emailSubject, emailContent)
+emailer.sendmail(recipient, emailSubject, emailContent, image)
